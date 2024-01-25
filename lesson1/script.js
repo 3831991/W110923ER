c701b37cf6a15016ed22f5f9edf651bf05774d28
Your Name <you@example.com>
2024-01-25 20:27:12 +0200

    added

--- lesson1/script.js
+++ lesson1/script.js
@@ -5,3 +5,9 @@ function welcomeMessage() {
 function showMessage() {
     console.log("ההודעה נשלחה בהצלחה");
 }
+
+function welcomeName() {
+    var name = "אלישיב";
+
+    alert("ברוך הבא " + name);
+}
