4a9978e353f8615509c8d89f5c9d705329b4bc48
Your Name <you@example.com>
2024-01-29 19:23:01 +0200

    added

--- lesson2/script.js
+++ lesson2/script.js
@@ -12,5 +12,12 @@ function multi() {
 }
 
 function plus() {
+    const n1 = +document.getElementById("num3").value;
+    const n2 = +document.getElementById("num4").value;
+
+    document.getElementById("output2").innerHTML = n1 + n2;
+}
+
+function plusResult() {
     
 }
