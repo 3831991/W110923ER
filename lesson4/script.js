0d7f40ff73d25137c23754855a6a56d30a5e5c57
Your Name <you@example.com>
2024-02-05 18:31:31 +0200

    added

---
+++ lesson4/script.js
@@ -0,0 +1,29 @@
+let counter = 1;
+
+function plus() {
+    // הגדלנו את המשתנה ב-1
+    counter++;
+    // שמנו את הערך ב-HTML
+    document.getElementById("count").innerHTML = counter;
+    // שיננו את גודל הכתב של המספר בהתאם לערך של המספר
+    document.getElementById("count").style.fontSize = counter + "em";
+}
+
+function minus() {
+    // הקטנו את המשתנה ב-1
+    counter--;
+    // שמנו את הערך ב-HTML
+    document.getElementById("count").innerHTML = counter;
+    // שיננו את גודל הכתב של המספר בהתאם לערך של המספר
+    document.getElementById("count").style.fontSize = counter + "em";
+}
+
+function cityChange() {
+    const city = document.getElementById("city").value;
+
+    if (city == "קרית ארבע") {
+        document.getElementById("output1").innerHTML = "נכון מאוד";
+    } else {
+        document.getElementById("output1").innerHTML = "טעות..";
+    }
+}
