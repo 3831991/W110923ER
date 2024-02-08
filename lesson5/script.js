2f38e48988ddb18af8ac87a94eeced5695663efe
Your Name <you@example.com>
2024-02-08 20:29:16 +0200

    added

--- lesson5/script.js
+++ lesson5/script.js
@@ -5,3 +5,12 @@ function counter(num) {
 
     document.getElementById("count").innerHTML = count;
 }
+
+function sum(num1, num2, num3) {
+    return `${num1} + ${num2} + ${num3} = ${num1 + num2 + num3}`;
+}
+
+function calcVat(num) {
+    return `ללא מע"מ: ${num} | כולל מע"מ: ${num * 1.17}`;
+}
+
