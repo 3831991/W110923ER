3b3028cd89b43c6dd91107601f0d2ed68576c0b5
Your Name <you@example.com>
2024-02-15 18:32:17 +0200

    added

--- lesson6/script.js
+++ lesson6/script.js
@@ -52,3 +52,55 @@ function task3() {
 
     document.getElementById("output3").innerHTML = result;
 }
+
+function task4() {
+    const n1 = +document.getElementById("num2").value;
+    const n2 = +document.getElementById("num3").value;
+    let result = '';
+
+    let start = Math.min(n1, n2);
+    let end = Math.max(n1, n2);
+
+    while (start <= end) {
+        result += start + ', ';
+
+        start++;
+    }
+
+    document.getElementById("output4").innerHTML = result;
+}
+
+function task5() {
+    const num = +document.getElementById("num4").value;
+    let result = '';
+
+    for (let i = 1; i <= num; i++) {
+        if (num % i == 0) {
+            result += i + ', ';
+        }
+    }
+
+    document.getElementById("output5").innerHTML = result;
+}
+
+function task6() {
+    const num = +document.getElementById("num5").value;
+    let result = '';
+
+    for (let i = 0; i < num; i++) {
+        result += '* ';
+    }
+
+    document.getElementById("output6").innerHTML = result;
+}
+
+function task7() {
+    const num = +document.getElementById("num6").value;
+    let result = '';
+
+    for (let i = 0; i < num; i++) {
+        result += '*<br>';
+    }
+
+    document.getElementById("output7").innerHTML = result;
+}
