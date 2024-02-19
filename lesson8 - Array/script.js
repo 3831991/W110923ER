39909f6a8d8dceffd65e1d0ebe22c04392bd4abf
Your Name <you@example.com>
2024-02-19 20:16:51 +0200

    added

--- lesson8 - Array/script.js
+++ lesson8 - Array/script.js
@@ -1,4 +1,16 @@
 const div = document.querySelector('#numbers');
+const output = document.querySelector('#output');
 
 div.innerHTML = numbers.join(', ');
 
+function max() {
+    let num = numbers[0];
+
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > num) {
+            num = numbers[i];
+        }
+    }
+
+    output.innerHTML = `<b>המספר הגדול הוא:</b> ${num}`;
+}
