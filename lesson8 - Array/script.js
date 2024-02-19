a706aa926b03792dde3949daa8bb9a835da77742
Your Name <you@example.com>
2024-02-19 20:52:28 +0200

    added

--- lesson8 - Array/script.js
+++ lesson8 - Array/script.js
@@ -7,10 +7,52 @@ function max() {
     let num = numbers[0];
 
     for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] > num) {
-            num = numbers[i];
+        const currentNumber = numbers[i];
+
+        if (currentNumber > num) {
+            num = currentNumber;
         }
     }
 
     output.innerHTML = `<b>המספר הגדול הוא:</b> ${num}`;
 }
+
+function min() {
+    let num = numbers[0];
+
+    for (let i = 0; i < numbers.length; i++) {
+        const currentNumber = numbers[i];
+
+        if (currentNumber < num) {
+            num = currentNumber;
+        }
+    }
+
+    output.innerHTML = `<b>המספר הקטן הוא:</b> ${num}`;
+}
+
+function sum() {
+    let res = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        res += numbers[i];
+    }
+
+    output.innerHTML = `<b>הסכום הוא:</b> ${res}`;
+}
+
+function count() {
+    output.innerHTML = `<b>הכמות היא:</b> ${numbers.length}`;
+}
+
+function avg() {
+    let res = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        res += numbers[i];
+    }
+
+    const average = Math.round(res / numbers.length);
+    output.innerHTML = `<b>הממוצע הוא:</b> ${average}`;
+}
+
