accffaf24ef20ee4bbfcfdc681d6b625dfd8923e
Your Name <you@example.com>
2024-02-22 20:21:11 +0200

    added

--- lesson9 - DOM/tasks/script.js
+++ lesson9 - DOM/tasks/script.js
@@ -3,15 +3,28 @@ const tasks = document.querySelector(".tasks");
 function add() {
     const input = document.querySelector('input');
 
+    if (!input.value) {
+        return;
+    }
+
     const li = document.createElement('li');
     li.innerHTML = input.value;
-    tasks.appendChild(li);
+    li.addEventListener('click', removeTask);
 
+    tasks.appendChild(li);
     input.value = '';
 }
 
 function keyup(ev) {
     if (ev.key == 'Enter') {
         add();
+    }
+}
+
+function removeTask(ev) {
+    const isAllowed = confirm(`האם אתה בטוח כי ברצונך למחוק את ${ev.target.innerHTML}?`);
+
+    if (isAllowed) {
+        ev.target.remove();
     }
 }
