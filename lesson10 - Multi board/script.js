9d3f0b3b1e124ccffb2ff0f4c5c16ce52ef9ac15
Your Name <you@example.com>
2024-02-26 18:43:37 +0200

    added

--- lesson10 - Multi board/script.js
+++ lesson10 - Multi board/script.js
@@ -1,11 +1,16 @@
+// יצירת אלמנט כללי שבתוכו יופיע לוח הכפל
 const board = document.createElement('div');
 board.className = 'board';
+// הוספת האלמנט ל-Body
 document.body.appendChild(board);
 
 for (let y = 1; y <= 10; y++) {
     for (let x = 1; x <= 10; x++) {
+        // יצירת אלמנט שבתוכו יופיע המספר (כמו תא בטבלה)
         const div = document.createElement('div');
         div.innerHTML = x * y;
         board.appendChild(div);
+
+        div.setAttribute("ziv", `${y} x ${x} = ${y * x}`);
     }
 }
