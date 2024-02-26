7546e9e21499fd52cab4f7b3c973fc38fa3a65b9
Your Name <you@example.com>
2024-02-26 21:19:24 +0200

    added

--- lesson10 - DOM + Timing/Timing/script.js
+++ lesson10 - DOM + Timing/Timing/script.js
@@ -3,3 +3,16 @@ function task1(elem) {
         elem.style.display = 'none';
     }, 1000);
 }
+
+let counter = 1;
+let myInterval;
+
+function task2(elem) {
+    if (myInterval) {
+        myInterval = clearInterval(myInterval);
+    } else {
+        myInterval = setInterval(function() {
+            elem.innerHTML = ++counter;
+        }, 100);
+    }
+}
