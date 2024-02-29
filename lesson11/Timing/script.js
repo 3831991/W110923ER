7698eaa90fe3e82b1dc811e537f65fcd85a17d4a
Your Name <you@example.com>
2024-02-29 19:10:42 +0200

    added

---
+++ lesson11/Timing/script.js
@@ -0,0 +1,44 @@
+const traffic1 = document.querySelector('.traffic-light');
+
+const first = traffic1.querySelector('div');
+const second = traffic1.querySelector('div:nth-child(2)');
+const last = traffic1.querySelector('div:last-child');
+
+let trafficTimeOut;
+
+function reset() {
+    first.style.backgroundColor = '';
+    second.style.backgroundColor = '';
+    last.style.backgroundColor = '';
+}
+
+function green() {
+    reset();
+    last.style.backgroundColor = 'green';
+    trafficTimeOut = setTimeout(yellow, 3 * 1000);
+}
+
+function yellow() {
+    reset();
+    second.style.backgroundColor = 'yellow';
+    trafficTimeOut = setTimeout(red, 1000);
+}
+
+function red() {
+    reset();
+    first.style.backgroundColor = 'red';
+    trafficTimeOut = setTimeout(redAndYellow, 3 * 1000);
+}
+
+function redAndYellow() {
+    reset();
+    second.style.backgroundColor = 'yellow';
+    first.style.backgroundColor = 'red';
+    trafficTimeOut = setTimeout(green, 1000);
+}
+
+function stop() {
+    clearTimeout(trafficTimeOut);
+}
+
+red();
