a9a9b5faa38da7f3e18b247a0ab30389b1bf859f
Your Name <you@example.com>
2024-03-07 20:51:17 +0200

    added

--- lesson13/script.js
+++ lesson13/script.js
@@ -3,11 +3,14 @@ const tbody = document.querySelector('tbody');
 for (const p of products) {
     const tr = document.createElement('tr');
 
+    const finalPrice = p.price - (p.price * p.discount / 100);
+
     tr.innerHTML = `
         <td>${p.id}</td>
         <td>${p.name}</td>
         <td style="font-size: ${p.price}px">${p.price}</td>
         <td>${p.discount || 0}%</td>
+        <td>${Math.round(finalPrice)}</td>
         <td>${p.madeIn}</td>
         <td style="background-color: ${p.color}">${p.color}</td>
     `;
