cdb3d373f9ff18fa89b6c60a7f2c98e6a7a1eb28
Your Name <you@example.com>
2024-03-11 18:07:48 +0200

    added

---
+++ lesson14/tasks/script.js
@@ -0,0 +1,34 @@
+const tasks = document.querySelector(".tasks");
+
+// פונקציה להוספת אלמנטים
+function newTask() {
+    // יצירת אלמנט חדש
+    const li = document.createElement('li');
+
+    // הוספת האלמנט לתור הרשימה
+    tasks.appendChild(li);
+
+    // השמת התוכן של המשימה לאלמנט החדש
+    const div = document.createElement('div');
+
+    // מאפשרים למשתמש לערוך את התוכן של האלמנט
+    div.contentEditable = true;
+    li.appendChild(div);
+
+    // יצירת לחצן מחיקה
+    const btn = document.createElement('button');
+    btn.innerHTML = 'x';
+
+    // יצירת אירוע - כל לחיצה על הלחצן תפעיל את הפונקציה של המחיקה
+    btn.addEventListener('click', function() {
+        const isAllowed = confirm(`האם אתה בטוח כי ברצונך למחוק את ${div.innerHTML}?`);
+
+        if (isAllowed) {
+            li.remove();
+        }
+    });
+    li.appendChild(btn);
+
+    // איפוס תיב הקלט
+    input.value = '';
+}
