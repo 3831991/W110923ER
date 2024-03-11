73011f63d6842aa25ed283aa629800188307eb85
Your Name <you@example.com>
2024-03-11 21:24:27 +0200

    added

--- lesson14/page-builder/script.js
+++ lesson14/page-builder/script.js
@@ -7,3 +7,15 @@ function bgChange(elem) {
 function paddingChange(elem) {
     page.style.padding = elem.value + 'px';
 }
+
+function pageToShow(id, elem) {
+    const actives = document.querySelectorAll('nav a.active');
+
+    for (const a of actives) {
+        a.classList.remove('active');
+    }
+
+    elem.classList.add('active');
+
+    
+}
