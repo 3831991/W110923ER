e68137d3fa83640ea68fdd96e4f35dff3dba4d65
Your Name <you@example.com>
2024-03-11 21:08:49 +0200

    added

--- lesson14/page-builder/script.js
+++ lesson14/page-builder/script.js
@@ -3,3 +3,7 @@ const page = document.querySelector("#page");
 function bgChange(elem) {
     page.style.backgroundColor = elem.value;
 }
+
+function paddingChange(elem) {
+    page.style.padding = elem.value + 'px';
+}
