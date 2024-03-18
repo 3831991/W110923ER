a1588217ab14a07e1bf5414900ae115dbd4d9ab1
Your Name <you@example.com>
2024-03-18 21:09:29 +0200

    added

--- lesson16/tic tac toe/script.js
+++ lesson16/tic tac toe/script.js
@@ -1,10 +1,15 @@
 const divs = document.querySelectorAll("#board>div");
 let isX = true;
+let isGameOver = false;
 
 // לולאה העוברת על כל המשבצות
 divs.forEach(div => {
     // הוספת פונקציה המופעלת בעת לחיצה על אחת המשבצות
     div.addEventListener("click", function(ev) {
+        if (isGameOver) {
+            return;
+        }
+
         // האלמנט שעליו לחץ השחקן
         const elem = ev.target;
 
@@ -23,6 +28,7 @@ divs.forEach(div => {
         isX = !isX;
         // הפעלת הפונקציה של המחווה הויזואלית
         showTurn();
+        checkWinner();
     });
 })
 
@@ -39,4 +45,38 @@ function showTurn() {
     } else {
         document.querySelector("#players>div:last-child").classList.add('currentTurn');
     }
+}
+
+function checkWinner() {
+    // מערך של מערכים של מיקומים אפשריים לניצחון
+    const options = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    // רץ על המערך של כל האופציות
+    for (const op of options) {
+        // בודק את המיקומים של כל מערך
+        if (op.every(myIndex => divs[myIndex].innerText === 'X')) {
+            winner(op, 'X');
+            break;
+        } else if (op.every(myIndex => divs[myIndex].innerText === 'O')) {
+            winner(op, 'O');
+            break;
+        }
+    }
+}
+
+function winner(op, win) {
+    setTimeout(() => alert(win + " is winner!"), 50);
+
+    op.forEach(x => divs[x].classList.add('win'));
+
+    isGameOver = true;
 }
