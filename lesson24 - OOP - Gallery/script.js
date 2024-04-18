484ca71647e4370f7976a43f84bc73783efd4812
Your Name <you@example.com>
2024-04-18 18:59:18 +0300

    added

--- lesson24 - OOP - Gallery/script.js
+++ lesson24 - OOP - Gallery/script.js
@@ -40,5 +40,6 @@ class Gallery {
 }
 
 
-new Gallery("gallery", "תמונה (1).jpg", "תמונה (2).jpg", "תמונה (12).jpg");
-new Gallery("gallery", "תמונה (5).jpg", "תמונה (7).jpg");
+new Gallery("gallery", "תמונה (1).jpg", "תמונה (2).jpg", "תמונה (3).jpg", "תמונה (4).jpg", "תמונה (5).jpg");
+new Gallery("gallery", "תמונה (11).jpg", "תמונה (12).jpg", "תמונה (13).jpg", "תמונה (14).jpg", "תמונה (15).jpg", "תמונה (16).jpg", "תמונה (17).jpg");
+new Gallery("gallery", "תמונה (6).jpg", "תמונה (7).jpg", "תמונה (8).jpg", "תמונה (9).jpg", "תמונה (10).jpg");
