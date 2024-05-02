cd9516968939385c92368d18ef63eab21136a352
Your Name <you@example.com>
2024-05-02 18:40:58 +0300

    added

--- lesson25 - snake/script.js
+++ lesson25 - snake/script.js
@@ -1,6 +1,8 @@
 const board = document.querySelector("#board");
 const width = 30;
 const height = 30;
+const snake = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+const divs = [];
 
 function createBoard() {
     board.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
@@ -9,5 +11,18 @@ function createBoard() {
         const div = document.createElement("div");
         div.innerText = i;
         board.appendChild(div);
+        divs.push(div);
     }
+
+    color();
+}
+
+function color() {
+    divs.forEach(div => {
+        div.classList.remove("snake");
+    });
+
+    snake.forEach(x => {
+        divs[x].classList.add("snake");
+    });
 }
