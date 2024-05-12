760bef19f48aa76f6a9aca3424d5aa6b437f77a3
Your Name <you@example.com>
2024-05-12 15:26:05 +0300

    fix

--- lesson24 - OOP/gallery/script.js
+++ lesson24 - OOP/gallery/script.js
@@ -4,7 +4,8 @@
  * @param imagesArr שמות של תמונות שיוצגו בגלרייה
  */
 class Gallery {
-    images = [];
+    static vat = 17;
+    #images = [];
     imgElem = document.createElement("img");
     currentImage = 0;
 
@@ -12,7 +13,7 @@ class Gallery {
         // חיבור לאלמנט של הגלרייה (לפי שם המזהה שקיבלנו)
         const galleryElem = document.getElementById(elemId);
         // שם את שמות התמונות שקיבלנו בתוך המחלקה
-        this.images = imagesArr;
+        this.#images = imagesArr;
 
         galleryElem.appendChild(this.imgElem);
         this.showImage();
@@ -26,7 +27,7 @@ class Gallery {
         this.currentImage++;
 
         // אם מיקום התמונה גדול או שווה לאורך המערך, מחזירים את המיקום ל-0 (מתחילים מהתחלה)
-        if (this.currentImage >= this.images.length) {
+        if (this.currentImage >= this.#images.length) {
             this.currentImage = 0;
         }
 
@@ -35,10 +36,10 @@ class Gallery {
 
     showImage() {
         // מחליפים את התמונה בגלרייה לפי המיקום החדש
-        this.imgElem.src = `./images/${this.images[this.currentImage]}`;
+        this.imgElem.src = `./images/${this.#images[this.currentImage]}`;
     }
 }
 
 new Gallery("gallery", "תמונה (1).jpg", "תמונה (2).jpg", "תמונה (3).jpg", "תמונה (4).jpg", "תמונה (5).jpg");
 new Gallery("gallery", "תמונה (11).jpg", "תמונה (12).jpg", "תמונה (13).jpg", "תמונה (14).jpg", "תמונה (15).jpg", "תמונה (16).jpg", "תמונה (17).jpg");
-new Gallery("gallery", "תמונה (6).jpg", "תמונה (7).jpg", "תמונה (8).jpg", "תמונה (9).jpg", "תמונה (10).jpg");
+const g = new Gallery("gallery", "תמונה (6).jpg", "תמונה (7).jpg", "תמונה (8).jpg", "תמונה (9).jpg", "תמונה (10).jpg");
